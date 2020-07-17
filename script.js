8de85db58a45fba5d59afbe3a2b145321c052481
Glitch (hello-webpage) <none>
2020-07-17 10:43:03 +0000

    🕵🏖 Checkpoint ./script.js:3168504/966 ./index.html:3168504/60

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var c = document.getElementById("preview");
 var ctx = c.getContext("2d");
 
+let x = 70;
+let y = 120;
+
 ctx.fillStyle = "pink";
 ctx.fillRect(0, 0, c.width, c.height);
 
 ctx.fillStyle = "black";
 ctx.font = "40px Arial";
-ctx.fillText("Hi, I am", 50, 70);
+ctx.fillText("Hi 👋, I'm", x, y);
+
+ctx.font = "bold 70px Arial";
+ctx.fillText("Khaleel Gibran 🍩", x-3, y+70);
+
+function save() {
+  var canvas = document.getElementById("mycanvas");
+  var img = canvas.toDataURL("image/png");
+  document.getElementById('preview-img').href = img;
+}
