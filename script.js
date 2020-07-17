c170676837851f51a34a77c5535ea32955c6915e
Glitch (hello-webpage) <none>
2020-07-17 11:35:07 +0000

    🐀📹 Checkpoint ./style.css:3168504/890 ./index.html:3168504/774 ./script.js:3168504/1801

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-var c = document.getElementById("preview");
-var ctx = c.getContext("2d");
+var app = new Vue({
+  data: function() {
+    return {
+      color: "black",
+      background: "white",
+      username: "User"
+    }
+  },
+  methods: {
+    updateCanvas() {
+      var c = document.getElementById("preview");
+      var ctx = c.getContext("2d");
 
-let x = 70;
-let y = 120;
+      let x = 70;
+      let y = 120;
 
-ctx.fillStyle = "pink";
-ctx.fillRect(0, 0, c.width, c.height);
+      ctx.fillStyle = "pink";
+      ctx.fillRect(0, 0, c.width, c.height);
 
-ctx.fillStyle = "black";
-ctx.font = "40px Arial";
-ctx.fillText("Hi 👋, I'm", x, y);
+      ctx.fillStyle = "black";
+      ctx.font = "40px Arial";
+      ctx.fillText("Hi 👋, I'm", x, y);
 
-ctx.font = "bold 70px Arial";
-ctx.fillText("Khaleel Gibran 🍩", x-3, y+70);
+      ctx.font = "bold 70px Arial";
+      ctx.fillText("Khaleel Gibran 🍩", x-3, y+70);
+    }
+  }
+})
+
+
+function updateCanvas() {
+  
+}
 
 function save() {
   var canvas = document.getElementById("preview");
