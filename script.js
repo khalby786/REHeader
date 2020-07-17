5fb504984d8e20abc9e1d69605a9cbe2ee03b6be
Glitch (hello-webpage) <none>
2020-07-17 10:32:02 +0000

    👩‍❤️‍💋‍👩🚖 Checkpoint ./style.css:3168504/168 ./index.html:3168504/575 ./script.js:3168504/996

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var c = document.getElementById("preview");
+var ctx = c.getContext("2d");
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+ctx.fillStyle = "pink";
+ctx.fillRect(0, 0, c.width, c.height);
+
+ctx.fillStyle = "black";
+ctx.font = "40px Arial";
+ctx.fillText("Hi, I am", 50, 70);
